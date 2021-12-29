86069b1c732f55dca08203f8e46e272e1d75f966
coppe <coppepam@gmail.com>
2021-12-29 15:52:34 +0900

    actionとhorrorが空配列で来る件修正

--- src/request.ts
+++ src/request.ts
@@ -1,12 +1,13 @@
 const API_KEY:string = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
 export const requests = {
+    fetchGenreList       : `/genre/movie/list?api_key=${API_KEY}`,
     feachTrending        : `/trending/all/week?api_key=${API_KEY}&language=en-us`,
-    feachNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    feactTopRated        : `/discover/tv?api_key=${API_KEY}&languager=en-us`,
-    feactActionMovies    : `/discover/tv?api_key=${API_KEY}&with_genres=28`,
-    feactComedyMovies    : `/discover/tv?api_key=${API_KEY}&with_genres=35`,
-    feactHorrorMovies    : `/discover/tv?api_key=${API_KEY}&with_genres=27`,
-    feactRomanceMovies   : `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
-    feactDocumentMovies  : `/discover/tv?api_key=${API_KEY}&with_genres=99`,
+    feachNetflixOriginals: `/discover/movie?api_key=${API_KEY}&with_networks=213`,
+    feactTopRated        : `/discover/movie?api_key=${API_KEY}&languager=en-us`,
+    feactActionMovies    : `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    feactComedyMovies    : `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    feactHorrorMovies    : `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    feactRomanceMovies   : `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    feactDocumentMovies  : `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 }
